c76c54a47ea6e45ab3d774117a33ca08e519cc8c
opetrocheilos <opetrocheilos@legion.com>
2018-10-08 22:56:40 +0100

    renamed output dll and added version information.

--- C#/Properties/AssemblyInfo.cs
+++ C#/Properties/AssemblyInfo.cs
@@ -2,35 +2,32 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinFormsControls")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VS_Build Timer")]
+[assembly: AssemblyDescription("Visual Studio Build Timer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinFormsControls")]
+[assembly: AssemblyProduct("Microsoft.Samples.VisualStudio.IDE.ToolWindow.Properties")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f540a15f-5140-47e1-93a1-62ae66dafb32")]
+[assembly: Guid("b4f70329-ebc8-44c1-af1e-aec328af797a")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.*")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
