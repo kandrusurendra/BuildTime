d664f4d284a9411873f87b191262cf6441f48dcb
Odysseas <odyxanthi@gmail.com>
2018-12-26 20:19:02 +0000

    Set version to 0.1.0.12262

--- VS_BuildTimer/Properties/AssemblyInfo.cs
+++ VS_BuildTimer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.12261")]
-[assembly: AssemblyFileVersion("0.1.0.12261")]
+[assembly: AssemblyVersion("0.1.0.12262")]
+[assembly: AssemblyFileVersion("0.1.0.12262")]
