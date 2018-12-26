97aaeed8fc2c7a2dad875fbd8647de35d0fce2c5
opetrocheilos <opetrocheilos@legion.com>
2018-12-26 20:38:53 +0000

    removed visual studio express from install targets and incremented build number.

--- VS_BuildTimer/Properties/AssemblyInfo.cs
+++ VS_BuildTimer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.12262")]
-[assembly: AssemblyFileVersion("0.1.0.12262")]
+[assembly: AssemblyVersion("0.1.0.12263")]
+[assembly: AssemblyFileVersion("0.1.0.12263")]
