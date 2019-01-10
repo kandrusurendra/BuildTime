42bbb432b11420691e18352ec8ced23091720b65
opetrocheilos <opetrocheilos@legion.com>
2019-01-10 00:48:39 +0000

    incremented assembly version to v0.1.0.1101

--- VS_BuildTimer/Properties/AssemblyInfo.cs
+++ VS_BuildTimer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.1.12282")]
-[assembly: AssemblyFileVersion("0.1.1.12282")]
+[assembly: AssemblyVersion("0.2.0.1101")]
+[assembly: AssemblyFileVersion("0.2.0.1101")]
